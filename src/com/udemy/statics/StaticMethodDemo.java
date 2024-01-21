e77e0f075e5017f0aa0de1099b3a03fc09ed5ccf
Aajesh <ajeshchoudhary@gmail.com>
2024-01-21 23:37:15 +0530

    Static member execution flow

--- src/com/udemy/statics/StaticMethodDemo.java
+++ src/com/udemy/statics/StaticMethodDemo.java
@@ -2,6 +2,17 @@ package com.udemy.statics;
 
 public class StaticMethodDemo {
     public static void main(String[] args){
-        System.out.println("static method Demo Testing ");
+        System.out.println(" main method ");
+        StaticMethodDemo.staticMethod1();
+    }
+
+    public static void staticMethod1(){
+        System.out.println("static method 1");
+    }
+
+    static {
+        System.out.println("static block1 ");
+        StaticMethodDemo.staticMethod1();
+
     }
 }
