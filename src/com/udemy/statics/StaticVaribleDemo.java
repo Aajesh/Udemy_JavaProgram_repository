b26f6e6c692ffcde1b2b9f810b051b87ecd2244e
Aajesh <ajeshchoudhary@gmail.com>
2024-01-21 23:38:20 +0530

    Static member execution flow...

---
+++ src/com/udemy/statics/StaticVaribleDemo.java
@@ -0,0 +1,9 @@
+package com.udemy.statics;
+
+public class StaticVaribleDemo {
+
+    static int num;
+    public static void main(String[] args) {
+        System.out.println(StaticVaribleDemo.num);
+    }
+}
